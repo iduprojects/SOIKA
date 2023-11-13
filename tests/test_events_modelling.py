e7dce6e4372b559c957a52667a3b14256496df08
GeorgeKontsevik <george.kontsevik@gmail.com>
2023-11-13 18:07:45 +0300

    fixed some make and poetry-related files

--- tests/test_events_modelling.py
+++ tests/test_events_modelling.py
@@ -8,11 +8,13 @@ from factfinder import EventDetection
 path_to_population = "data/raw/population.geojson"
 path_to_data = "data/processed/messages.geojson"
 
+
 @pytest.fixture
 def gdf():
     gdf = gpd.read_file(path_to_data)
     gdf = gdf.head(6)
     return gdf
+
 
 def test_event_detection(gdf):
     expected_name = "0_фурштатская_штукатурного слоя_слоя_отслоение"
@@ -20,11 +22,13 @@ def test_event_detection(gdf):
     expected_messages = [4, 5, 3, 2]
     event_model = EventDetection()
     messages, events, connections = event_model.run(
-        gdf, path_to_population, 'Санкт-Петербург', 32636, min_event_size=3
-        )
-    event_name = events.iloc[0]['name']
-    event_risk = events.iloc[0]['risk'].round(3)
-    event_messages = [int(mid) for mid in events.iloc[0]['message_ids'].split(', ')]
+        gdf, path_to_population, "Санкт-Петербург", 32636, min_event_size=3
+    )
+    event_name = events.iloc[0]["name"]
+    event_risk = events.iloc[0]["risk"].round(3)
+    event_messages = [
+        int(mid) for mid in events.iloc[0]["message_ids"].split(", ")
+    ]
     assert event_name == expected_name
     assert event_risk == expected_risk
     assert all(mid in event_messages for mid in expected_messages)
--- tests/test_location.py
+++ tests/test_location.py
@@ -16,16 +16,13 @@ from factfinder.src.geocoder import Geocoder
 #     result = Location().query(input_address)
 #     assert result.address == geocode_result
 
+
 @pytest.mark.parametrize(
     "input_address,geocode_result",
     [
-        (
-            'возле дома на Итальянской 17 постоянно мусорят!!!',
-            'Итальянской 17'
-        ),
+        ("возле дома на Итальянской 17 постоянно мусорят!!!", "Итальянской 17"),
     ],
 )
-
 def test_geolocator(input_address, geocode_result):
     result = Geocoder().extract_ner_street(input_address)
     assert result.loc[0] == geocode_result
